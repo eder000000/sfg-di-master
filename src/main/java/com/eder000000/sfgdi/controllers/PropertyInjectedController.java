844362c075ea93ccdcaee0313083a4bb01f33b67
edera <edera@amdocs.com>
2022-09-21 12:52:32 -0500

    Dependency Injection by properties

---
+++ src/main/java/com/eder000000/sfgdi/controllers/PropertyInjectedController.java
@@ -0,0 +1,12 @@
+package com.eder000000.sfgdi.controllers;
+
+import com.eder000000.sfgdi.services.GreetingService;
+
+public class PropertyInjectedController {
+
+    public GreetingService greetingService;
+
+    public String getGreeting(){
+        return greetingService.sayGreeting();
+    }
+}
---
+++ src/main/java/com/eder000000/sfgdi/services/GreetingService.java
@@ -0,0 +1,5 @@
+package com.eder000000.sfgdi.services;
+
+public interface GreetingService {
+    String sayGreeting();
+}
