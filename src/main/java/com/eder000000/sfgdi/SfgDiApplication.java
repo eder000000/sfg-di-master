5477cba53c631f441cdcff77e97e2837610d7e78
edera <edera@amdocs.com>
2022-09-21 11:18:15 -0500

    Spring context

--- src/main/java/com/eder000000/sfgdi/SfgDiApplication.java
+++ src/main/java/com/eder000000/sfgdi/SfgDiApplication.java
@@ -1,13 +1,23 @@
 package com.eder000000.sfgdi;
 
+import com.eder000000.sfgdi.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class SfgDiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SfgDiApplication.class, args);
+
+		ApplicationContext ctx =  SpringApplication.run(SfgDiApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
---
+++ src/main/java/com/eder000000/sfgdi/controllers/MyController.java
@@ -0,0 +1,12 @@
+package com.eder000000.sfgdi.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+    public String sayHello(){
+        System.out.println("Hello!");
+        return "Hi folks!";
+    }
+}
