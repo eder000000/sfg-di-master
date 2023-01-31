5cb820c07fcb669ef2dcb5535db071b7fc98a293
edera <edera@amdocs.com>
2023-01-31 01:25:58 -0600

    Implement interfaces for services. Closes #4

---
+++ pet-clinic-data/src/main/java/sfgdi/services/VetService.java
@@ -0,0 +1,15 @@
+package sfgdi.services;
+
+import sfgdi.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+
+}
